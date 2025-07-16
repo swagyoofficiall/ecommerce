ff50312a142de094076ba77adb5d570802ef218b
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-16 20:54:03 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.GATSBY_SUPABASE_URL;
-const supabaseAnonKey = process.env.GATSBY_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
