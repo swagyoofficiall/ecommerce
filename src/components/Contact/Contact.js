b97d77f23f1dbf4946585cf2238fa1d65a42a475
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-19 17:14:39 +0530

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import Button from '../Button';
-
 import FormInputField from '../FormInputField/FormInputField';
-
 import * as styles from './Contact.module.css';
 
-const Contact = (props) => {
+const Contact = () => {
   const initialState = {
     name: '',
     phone: '',
@@ -31,67 +29,67 @@ const Contact = (props) => {
         <h4>Send Us A Message</h4>
         <p>
           Our Customer Service team are here for all enquiries Monday to Friday,
-          9am - 5pm AEDT (Australian Eastern Daylight Savings Time).
+          10am - 5pm AEDT (india inr Time).
         </p>
         <p>We look forward to hearing from you.</p>
       </div>
 
       <div className={styles.section}>
         <h4>Phone</h4>
-        <p>+1 424 280 4971</p>
-        <p>Monday to Friday - 9am - 5pm AEDT</p>
+        <p>+1 3156235096</p>
+        <p>Monday to Friday - 10am - 5pm AEDT</p>
       </div>
 
       <div className={styles.section}>
         <h4>Email</h4>
         <p>
-          You can email our Customer Service team at customerservice@example.com
+          You can email our Customer Service team at shopsupport@swagyo.com
           or via the contact form below:
         </p>
       </div>
 
       <div className={styles.contactContainer}>
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={handleSubmit}>
           <div className={styles.contactForm}>
             <FormInputField
-              id={'name'}
+              id="name"
               value={contactForm.name}
-              handleChange={(id, e) => handleChange(id, e)}
-              type={'text'}
-              labelName={'Full Name'}
+              handleChange={handleChange}
+              type="text"
+              labelName="Full Name"
               required
             />
             <FormInputField
-              id={'phone'}
+              id="phone"
               value={contactForm.phone}
-              handleChange={(id, e) => handleChange(id, e)}
-              type={'number'}
-              labelName={'Phone Number'}
+              handleChange={handleChange}
+              type="number"
+              labelName="Phone Number"
               required
             />
             <FormInputField
-              id={'email'}
+              id="email"
               value={contactForm.email}
-              handleChange={(id, e) => handleChange(id, e)}
-              type={'email'}
-              labelName={'Email'}
+              handleChange={handleChange}
+              type="email"
+              labelName="Email"
               required
             />
             <div className={styles.commentInput}>
               <FormInputField
-                id={'comment'}
+                id="comment"
                 value={contactForm.comment}
-                handleChange={(id, e) => handleChange(id, e)}
-                type={'textarea'}
-                labelName={'Comments / Questions'}
+                handleChange={handleChange}
+                type="textarea"
+                labelName="Comments / Questions"
                 required
               />
             </div>
           </div>
           <Button
             className={styles.customButton}
-            level={'primary'}
-            type={'buttonSubmit'}
+            level="primary"
+            type="submit"
           >
             submit
           </Button>
