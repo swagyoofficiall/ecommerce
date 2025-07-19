63189e73d3c7233c6c55bec60d2bf157aae630a5
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-19 15:56:27 +0530

    Update BlogPreviewGrid.js

--- src/components/BlogPreviewGrid/BlogPreviewGrid.js
+++ src/components/BlogPreviewGrid/BlogPreviewGrid.js
@@ -5,24 +5,22 @@ import BlogPreview from '../BlogPreview';
 
 const BlogPreviewGrid = (props) => {
   const { data, hideReadMoreOnWeb, showExcerpt } = props;
+
   return (
     <div className={styles.root}>
-      {data &&
-        data.map((blog, index) => {
-          return (
-            <BlogPreview
-              key={index}
-              image={blog.image}
-              altImage={blog.alt}
-              title={blog.title}
-              link={blog.link}
-              category={blog.category}
-              excerpt={blog.excerpt}
-              hideReadMoreOnWeb={hideReadMoreOnWeb}
-              showExcerpt={showExcerpt}
-            />
-          );
-        })}
+      {data?.map((blog, index) => (
+        <BlogPreview
+          key={index}
+          image={blog.image}
+          altImage={blog.alt}
+          title={blog.title}
+          link={blog.link}
+          category={blog.category}
+          excerpt={blog.excerpt}
+          hideReadMoreOnWeb={hideReadMoreOnWeb}
+          showExcerpt={showExcerpt}
+        />
+      ))}
     </div>
   );
 };
