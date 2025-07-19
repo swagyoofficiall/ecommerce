e326b1b4b3450b2fe8d3a54e6083cda77d60dc19
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-19 15:55:01 +0530

    Update BlogPreview.js

--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -4,21 +4,30 @@ import * as styles from './BlogPreview.module.css';
 import { toOptimizedImage } from '../../helpers/general';
 
 const BlogPreview = (props) => {
-  const { image, altImage, title, link, category, showExcerpt, excerpt } =
-    props;
+  const {
+    image,
+    altImage,
+    title,
+    link,
+    category,
+    showExcerpt,
+    excerpt,
+  } = props;
 
   const handleClick = () => {
     navigate(link);
   };
 
   return (
-    /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
-    <div className={styles.root} onClick={handleClick}>
+    <div
+      className={styles.root}
+      onClick={handleClick}
+      role="presentation"
+    >
       <img
         className={styles.blogPreviewImage}
-        alt={altImage}
         src={toOptimizedImage(image)}
-        role={'figure'}
+        alt={altImage}
       />
       <span className={styles.category}>{category}</span>
       <h4 className={styles.title}>
