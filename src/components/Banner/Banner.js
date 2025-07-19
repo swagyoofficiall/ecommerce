63e0107959cd4f8e40c7662bfc3e824cc328462a
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-19 15:50:26 +0530

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,33 +2,31 @@ import React from 'react';
 import * as styles from './Banner.module.css';
 import { toOptimizedImage } from '../../helpers/general';
 
-const Banner = (props) => {
-  const {
-    maxWidth,
-    name,
-    subtitle,
-    color,
-    bgImage,
-    height,
-    bgColor = 'var(--standard-light-grey)',
-    hideSubtitleOnMobile = true,
-  } = props;
-
+const Banner = ({
+  maxWidth,
+  name,
+  subtitle,
+  color,
+  bgImage,
+  height,
+  bgColor = 'var(--standard-light-grey)',
+  hideSubtitleOnMobile = true,
+}) => {
   const customStyling = {
     backgroundColor: bgColor,
-    backgroundImage: bgImage !== undefined ? `url(${toOptimizedImage(bgImage)})` : 'none',
-    height: height,
-    color: color,
+    backgroundImage: bgImage ? `url(${toOptimizedImage(bgImage)})` : 'none',
+    height,
+    color,
   };
 
   return (
     <div className={styles.root} style={customStyling}>
-      <div className={styles.content} style={{ maxWidth: maxWidth }}>
+      <div className={styles.content} style={{ maxWidth }}>
         <h2>{name}</h2>
         {subtitle && (
           <span
             className={`${styles.subtitle} ${
-              hideSubtitleOnMobile === true ? styles.hideSubtitleOnMobile : ''
+              hideSubtitleOnMobile ? styles.hideSubtitleOnMobile : ''
             }`}
           >
             {subtitle}
