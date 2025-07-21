bc7679578a1be9208453ac48c72695b93b182a82
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-21 22:55:26 +0530

    Update supabaseClient.js

--- src/helpers/supabaseClient.js
+++ src/helpers/supabaseClient.js
@@ -1,14 +1,6 @@
-// src/helpers/supabaseClient.js
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.GATSBY_SUPABASE_URL;
 const supabaseAnonKey = process.env.GATSBY_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase environment variables are not set');
-}
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
