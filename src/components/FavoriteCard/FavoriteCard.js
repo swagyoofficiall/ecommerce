a0a4fedde38cf7920b58b2253596ae6202d4a40b
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-21 13:04:58 +0530

    Update FavoriteCard.js

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -7,31 +7,42 @@ import * as styles from './FavoriteCard.module.css';
 import { toOptimizedImage } from '../../helpers/general';
 
 const FavoriteCard = (props) => {
+  const {
+    color,
+    size,
+    img,
+    alt,
+    product_name,
+    showConfirmDialog,
+  } = props;
+
   const [showQuickView, setShowQuickView] = useState(false);
-  const { color, size, img, alt, showConfirmDialog } = props;
+
   return (
     <div className={styles.root}>
       <div>
         <div className={styles.imageContainer}>
-          <img src={toOptimizedImage(img)} alt={alt} />
+          <img src={toOptimizedImage(img)} alt={alt || product_name} />
         </div>
         <div className={styles.metaContainer}>
+          <h4>{product_name}</h4>
           <span>Color: {color}</span>
           <span>Size: {size}</span>
         </div>
       </div>
+
       <div className={styles.actionContainer}>
-        <span role={'presentation'} onClick={() => setShowQuickView(true)}>
+        <span role="presentation" onClick={() => setShowQuickView(true)}>
           Edit
         </span>
-        <span role={'presentation'} onClick={showConfirmDialog}>
+        <span role="presentation" onClick={showConfirmDialog}>
           Remove
         </span>
       </div>
 
       <Drawer visible={showQuickView} close={() => setShowQuickView(false)}>
         <QuickView
-          buttonTitle={'update favorite'}
+          buttonTitle="Update Favorite"
           close={() => setShowQuickView(false)}
         />
       </Drawer>
