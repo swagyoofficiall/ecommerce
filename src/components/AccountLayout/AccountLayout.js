0dba0a59e3296c5beeb8d841a772fb7d3fd3a15a
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-21 22:07:07 +0530

    Update AccountLayout.js

--- src/components/AccountLayout/AccountLayout.js
+++ src/components/AccountLayout/AccountLayout.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import supabase from '../../lib/supabase';
+import { supabase } from '../../lib/supabase'; // ✅ FIXED
 
 import AccountNav from '../AccountNav';
 import Container from '../Container';
