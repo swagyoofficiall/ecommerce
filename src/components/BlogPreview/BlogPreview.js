79d955ecceaba56e57c0b1e81a254a2508e3309e
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-21 20:22:16 +0530

    Update BlogPreview.js

--- src/components/BlogPreview/BlogPreview.js
+++ src/components/BlogPreview/BlogPreview.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import { Link, navigate } from 'gatsby';
+import { Link } from 'gatsby';
 import * as styles from './BlogPreview.module.css';
 import { toOptimizedImage } from '../../helpers/general';
 
-const BlogPreview = (props) => {
-  const {
-    image,
-    altImage,
-    title,
-    link,
-    category,
-    showExcerpt,
-    excerpt,
-  } = props;
-
-  const handleClick = () => {
-    navigate(link);
-  };
-
+const BlogPreview = ({
+  image,
+  title,
+  slug,
+  category,
+  showExcerpt,
+  excerpt,
+}) => {
   return (
-    <div
-      className={styles.root}
-      onClick={handleClick}
-      role="presentation"
-    >
-      <img
-        className={styles.blogPreviewImage}
-        src={toOptimizedImage(image)}
-        alt={altImage}
-      />
-      <span className={styles.category}>{category}</span>
+    <div className={styles.root} role="presentation">
+      {image && (
+        <img
+          className={styles.blogPreviewImage}
+          src={toOptimizedImage(image)}
+          alt={title}
+        />
+      )}
+
+      {category && <span className={styles.category}>{category}</span>}
+
       <h4 className={styles.title}>
-        <Link to={link}>{title}</Link>
+        <Link to={`/blog/${slug}`}>{title}</Link>
       </h4>
-      {showExcerpt && <p className={styles.excerpt}>{excerpt}</p>}
+
+      {showExcerpt && excerpt && (
+        <p className={styles.excerpt}>{excerpt}</p>
+      )}
     </div>
   );
 };
