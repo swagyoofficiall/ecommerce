0b488b32138ad0a9faedf5e37f26e44a860e3748
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-22 21:51:43 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,7 @@
-require("dotenv").config({
-  path: `.env`,
-});
-
 module.exports = {
   siteMetadata: {
-    title: `Swagyo Shop`,
+    title: ` Ecommerce `,
     siteUrl: `https://shop.swagyo.com`,
   },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-env-variables',
-      options: {
-        allowList: ['GATSBY_SUPABASE_URL', 'GATSBY_SUPABASE_ANON_KEY'],
-      },
-    },
-    'gatsby-plugin-remove-serviceworker', // ✅ Make sure this is included
-  ],
+  plugins: [],
 };
