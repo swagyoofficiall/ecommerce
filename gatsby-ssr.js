08bfd458e92eeb53bc020a957579a11e859918a4
swagyoofficiall <swagyoofficial@gmail.com>
2025-07-27 19:24:51 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { NotificationProvider } from './src/context/AddItemNotificationProvider';
-import { CurrencyProvider } from './src/components/CurrencyFormatter/CurrencyContext';
 
 export const wrapRootElement = ({ element }) => (
-  <CurrencyProvider>
-    <NotificationProvider>
-      {element}
-    </NotificationProvider>
-  </CurrencyProvider>
+  <NotificationProvider>
+    {element}
+  </NotificationProvider>
 );
